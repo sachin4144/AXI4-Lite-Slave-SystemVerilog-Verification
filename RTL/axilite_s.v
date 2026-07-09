ac79090057ff888122f0d93a28ac0516d649dc7d
Sachin Kumar Mishra <149581959+sachin4144@users.noreply.github.com>
2026-07-09 12:01:19 +0530

    Update axilite_s.v

--- RTL/axilite_s.v
+++ RTL/axilite_s.v
@@ -227,19 +227,3 @@ begin
 end
 end
 endmodule
-
-
-interface axi_if;
-  logic clk,resetn;
-  logic awvalid, awready;
-  logic arvalid, arready;
-  logic wvalid, wready;
-  logic bready, bvalid;
-  logic rvalid, rready;
-  logic [31:0] awaddr, araddr, wdata, rdata;
-  logic [1:0] bresp, rresp;
-  
-endinterface
-
-
-
