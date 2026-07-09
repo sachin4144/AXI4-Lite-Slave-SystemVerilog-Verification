3d7e64b9a046a0c09c6b9b63cad3ab2aacade258
Sachin Kumar Mishra <149581959+sachin4144@users.noreply.github.com>
2026-07-09 11:50:29 +0530

    Create interface.sv

---
+++ Verification/interface.sv
@@ -0,0 +1,11 @@
+interface axi_if;
+  logic clk,resetn;
+  logic awvalid, awready;
+  logic arvalid, arready;
+  logic wvalid, wready;
+  logic bready, bvalid;
+  logic rvalid, rready;
+  logic [31:0] awaddr, araddr, wdata, rdata;
+  logic [1:0] bresp, rresp;
+  
+endinterface
